10dc64c11514eab1950127636947cd676dc38334
falamatt@amazon.com <falamatt@amazon.com>
2022-02-08 21:08:31 +0000

    Begin implementation of tiered event loop system. Blocked by libevent

---
+++ include/fluent-bit/flb_event.h
@@ -0,0 +1,258 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Monkey HTTP Server
+ *  ==================
+ *  Copyright 2001-2017 Eduardo Silva <eduardo@monkey.io>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <stdint.h>
+#include "mk_macros.h"
+#include "mk_list.h"
+#include "mk_bucket_queue.h"
+#include <fluent-bit/flb_mem.h>
+
+#ifndef MK_EVENT_H
+#define MK_EVENT_H
+
+/* Events type family */
+#define MK_EVENT_UNMODIFIED     -1    /* keep old event type              */
+#define MK_EVENT_NOTIFICATION    0    /* notification channel (pipe)      */
+#define MK_EVENT_LISTENER        1    /* listener socket                  */
+#define MK_EVENT_FIFO            2    /* FIFO - Messaging                 */
+#define MK_EVENT_CONNECTION      3    /* data on active connection        */
+#define MK_EVENT_CUSTOM          4    /* custom fd registered             */
+#define MK_EVENT_THREAD          5    /* thread-coroutine                 */
+
+/* Event triggered for file descriptors  */
+#define MK_EVENT_EMPTY           0
+#define MK_EVENT_READ            1
+#define MK_EVENT_WRITE           4
+#define MK_EVENT_SLEEP           8
+#define MK_EVENT_CLOSE          (16 | 8 | 8192)
+#define MK_EVENT_IDLE           (16 | 8)
+
+/* The event queue size */
+#define MK_EVENT_QUEUE_SIZE    256
+
+/* Events behaviors */
+#define MK_EVENT_LEVEL         256
+#define MK_EVENT_EDGE          512
+
+/* Event status */
+#define MK_EVENT_NONE            1    /* nothing */
+#define MK_EVENT_REGISTERED      2    /* event is registered into the ev loop */
+
+/* Priority bucket queue */
+#define MK_EVENT_PRIORITY_DEFAULT 6   /* default priority */
+
+/* Legacy definitions: temporal
+ *  ----------------------------
+ *
+ * Once a connection is dropped, define
+ * a reason.
+ */
+#define MK_EP_SOCKET_CLOSED   0
+#define MK_EP_SOCKET_ERROR    1
+#define MK_EP_SOCKET_TIMEOUT  2
+#define MK_EP_SOCKET_DONE     3
+/* ---- end ---- */
+
+#if defined(_WIN32)
+    #include "mk_event_libevent.h"
+#elif defined(MK_HAVE_EVENT_SELECT)
+    #include "mk_event_select.h"
+#elif defined(__linux__) && !defined(LINUX_KQUEUE)
+    #include "mk_event_epoll.h"
+#else
+    #include "mk_event_kqueue.h"
+#endif
+
+#if defined(_WIN32)
+    #define mk_event_closesocket(s) evutil_closesocket(s)
+#else
+    #define mk_event_closesocket(s) close(s)
+#endif
+
+/* Event reported by the event loop */
+struct mk_event {
+    int      fd;       /* monitored file descriptor */
+    int      type;     /* event type  */
+    uint32_t mask;     /* events mask */
+    uint8_t  status;   /* internal status */
+    void    *data;     /* custom data reference */
+
+    /* function handler for custom type */
+    int     (*handler)(void *data);
+    struct mk_list _head;
+    struct mk_list _priority_head;
+    char    priority;  /* optional priority */
+};
+
+struct mk_event_loop {
+    int size;                  /* size of events array */
+    int n_events;              /* number of events reported */
+    struct mk_event *events;   /* copy or reference of events triggered */
+    void *data;                /* mk_event_ctx_t from backend */
+};
+
+struct flb_event_loop {
+    int n_priorities;
+    struct mk_event_loop *primary_evl;     /* contains secondary evls */
+    struct mk_event_loop **secondary_evls; /* contains priority specific events */
+    struct mk_event *secondary_evl_events; /* events for evl */
+};
+
+/* create event loop */
+static inline struct flb_event_loop *flb_event_loop_create(int priorities, int size) {
+    struct flb_event_loop *evl;
+    int i;
+
+    /* create event loop */
+    evl = flb_calloc(sizeof(struct flb_event_loop *)); /* mk_event_loop_create(priorities); */
+    if (!evl) {
+        flb_errno();
+        return NULL;
+    }
+
+    /* load primary priority loop and register secondary loops */
+    evl->primary_evl = mk_event_loop_create(priorities);
+    if (!evl->primary_evl) {
+        flb_event_loop_destroy(evl);
+        return NULL;
+    }
+
+    /* initiate secondary priority loops */
+    evl->secondary_evls = priorities;
+    evl->secondary_evls = flb_calloc(priorities * sizeof(void *));
+    if (!evl->secondary_evls) {
+        flb_errno();
+        flb_event_loop_destroy(evl);
+        return NULL;
+    }
+    evl->secondary_evl_events = flb_malloc(priorities * sizeof(struct mk_event));
+
+    /* load secondary priority loops */
+    for(i = 0; i < priorities; ++i) {
+        evl->secondary_evls[i] = mk_event_loop_create(size);
+        if (!evl->secondary_evls[i]) {
+            flb_event_loop_destroy(evl);
+            return NULL;
+        }
+
+        /* register in primary evl */
+        MK_EVENT_NEW(&evl->secondary_evl_events[i]);
+        mk_event_add(evl->primary_evl, evl->secondary_evls[i]-> /* Blocker. no access to file descriptor on lib event... Windows. */
+    }
+
+    /* load primary priority loop and register secondary loops */
+    evl->primary_evl = mk_event_loop_create(priorities);
+    flb_event_loop_destroy(evl);
+
+    for (i = 0; i < priorities; ++i) {
+        evl->primary_evl;
+    }
+}
+
+static inline void flb_event_loop_destroy(struct flb_event_loop *evl)
+{
+    int i;
+    if (evl->secondary_evls) {
+        for (i = 0; (i < evl->n_priorities) && (evl->secondary_evls[i] != NULL); ++i) {
+            mk_event_loop_destroy(evl->secondary_evls[i]);
+        }
+        flb_free(evl->secondary_evls);
+    }
+    if (evl->primary_evl) {
+        mk_event_loop_destroy(evl->primary_evl);
+    }
+    if (evl->secondary_evl_events) {
+        flb_free(evl->secondary_evl_events);
+    }
+    flb_free(evl);
+}
+
+
+static inline void MK_EVENT_INIT(struct mk_event *ev, int fd, void *data,
+                                 int (*callback)(void *))
+{
+    ev->fd      = fd;
+    ev->type    = MK_EVENT_CUSTOM;
+    ev->mask    = MK_EVENT_EMPTY;
+    ev->status  = MK_EVENT_NONE;
+    ev->data    = data;
+    ev->handler = callback;
+}
+
+static inline void MK_EVENT_ZERO(struct mk_event *e)
+{
+    MK_EVENT_INIT(e, -1, NULL, NULL);
+}
+
+static inline void MK_EVENT_NEW(struct mk_event *e)
+{
+    e->mask   = MK_EVENT_EMPTY;
+    e->status = MK_EVENT_NONE;
+}
+
+int mk_event_init();
+int mk_event_initialize();
+struct mk_event_loop *mk_event_loop_create(int size);
+void mk_event_loop_destroy(struct mk_event_loop *loop);
+int mk_event_add(struct mk_event_loop *loop, int fd,
+                 int type, uint32_t mask, void *data);
+int mk_event_del(struct mk_event_loop *loop, struct mk_event *event);
+int mk_event_timeout_create(struct mk_event_loop *loop,
+                            time_t sec, long nsec,void *data);
+int mk_event_timeout_disable(struct mk_event_loop *loop, void *data);
+int mk_event_timeout_destroy(struct mk_event_loop *loop, void *data);
+int mk_event_channel_create(struct mk_event_loop *loop,
+                            int *r_fd, int *w_fd, void *data);
+int mk_event_wait(struct mk_event_loop *loop);
+int mk_event_wait_2(struct mk_event_loop *loop, int timeout);
+int mk_event_translate(struct mk_event_loop *loop);
+char *mk_event_backend();
+struct mk_event_fdt *mk_event_get_fdt();
+
+/* Having trouble getting this function inlined */
+void mk_event_load_bucket_queue(struct mk_event *event,
+                                      struct mk_bucket_queue *bktq,
+                                      struct mk_event_loop *evl);
+
+#define mk_event_priority_live_foreach(event, bktq, evl, max_iter)                      \
+    int __mk_event_priority_live_foreach_iter;  /* should enclose? */                   \
+    for (                                                                               \
+        /* init */                                                                      \
+        __mk_event_priority_live_foreach_iter = 0,                                      \
+        mk_event_load_bucket_queue(event, bktq, evl),                                   \
+        event = mk_bucket_queue_find_min(bktq) ?                                        \
+                mk_list_entry(                                                          \
+                    mk_bucket_queue_pop_min(bktq), struct mk_event, _priority_head) :   \
+                NULL;                                                                   \
+                                                                                        \
+        /* condition */                                                                 \
+        event != NULL &&                                                                \
+        (__mk_event_priority_live_foreach_iter < max_iter || max_iter == -1);           \
+                                                                                        \
+        /* update */                                                                    \
+        ++__mk_event_priority_live_foreach_iter,                                        \
+        mk_event_wait_2(evl, 0),                                                        \
+        mk_event_load_bucket_queue(event, bktq, evl),                                   \
+        event = mk_bucket_queue_find_min(bktq) ?                                        \
+                mk_list_entry(                                                          \
+                    mk_bucket_queue_pop_min(bktq), struct mk_event, _priority_head) :   \
+                NULL                                                                    \
+    )
+
+#endif
